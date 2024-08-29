aa78e6823e7cf425f32ac35898e9dfa02fbae91c
beesorlah-hub.Bisola Abdulsalam <beesorlah-hub@gmail.com>
2024-08-29 22:32:29 +0100

    health blog

--- .virtualenv/Scripts/activate.bat
+++ .virtualenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Adams\Desktop\Django project\health_blog\.virtualenv
+set VIRTUAL_ENV=C:\Users\Adams\Desktop\Django project\h_blog\.virtualenv
 
 if not defined PROMPT set PROMPT=$P$G
 
