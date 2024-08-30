0756ab58313484ce899bb1ed5fadcecbd24db5e2
beesorlah-hub.Bisola Abdulsalam <beesorlah-hub@gmail.com>
2024-08-30 01:40:26 +0100

    updated_requirements

--- Web/settings.py
+++ Web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
